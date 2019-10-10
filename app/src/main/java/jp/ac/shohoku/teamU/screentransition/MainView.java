5525cc547d87687342494159285c930f40bd8855
u-taisuke <utsumi@shohoku.ac.jp>
2019-10-11 01:57:05 +0900

    カスタムビューを作成しました。
     Conflicts:
    	app/src/main/java/jp/ac/shohoku/teamU/screentransition/MainView.java

--- app/src/main/java/jp/ac/shohoku/teamU/screentransition/MainView.java
+++ app/src/main/java/jp/ac/shohoku/teamU/screentransition/MainView.java
@@ -14,7 +14,7 @@ import androidx.annotation.Nullable;
 public class MainView extends View {
     public final int FIRST = 1;  //状態を表す定数 1
     public final int SECOND = 2; //状態を表す定数 2
-    int state;  //状態を表す変数
+    int state = FIRST;  //状態を表す変数
 
     public MainView(Context context) {
         super(context);
