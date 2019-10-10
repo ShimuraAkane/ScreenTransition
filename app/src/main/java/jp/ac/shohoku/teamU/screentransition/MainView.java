d3b698030f29404b75f441ee47dcd25a5d555fb8
TakebayashiiHaruka <s18b710@shohoku.ac.jp>
2019-10-10 15:51:12 +0900

    メインビューのonDrawメソッドを作りました。

--- app/src/main/java/jp/ac/shohoku/teamU/screentransition/MainView.java
+++ app/src/main/java/jp/ac/shohoku/teamU/screentransition/MainView.java
@@ -1,12 +1,20 @@
 package jp.ac.shohoku.teamU.screentransition;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class MainView extends View {
+    public final int FIRST = 1;  //状態を表す定数 1
+    public final int SECOND = 2; //状態を表す定数 2
+    int state;  //状態を表す変数
+
     public MainView(Context context) {
         super(context);
     }
@@ -21,5 +29,22 @@ public class MainView extends View {
 
     public MainView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        if (state == FIRST) {  //状態1の場合の描画
+            p.setColor(Color.BLUE);
+            canvas.drawARGB(255, 255, 255, 255);
+            canvas.drawRect(100, 100, 300, 200, p);
+        } else if (state == SECOND) {  //状態2の場合の描画
+            p.setColor(Color.RED);
+            canvas.drawARGB(255, 255, 255, 0);
+            canvas.drawRect(100, 100, 300, 200, p);
+        } else {  //それ以外の場合は、Logにエラーを吐き出す
+            Log.d("error", "never come here");
+        }
     }
 }
